969f5090f5e1d6828b2bd0b1dc638352b35894be
Mateo Difranco <mateodifr@gmail.com>
2026-07-11 19:08:00 -0300

    arreglo la pista para que respete los acentos

--- src/components/Hint.jsx
+++ src/components/Hint.jsx
@@ -1,10 +1,11 @@
+import { ultimaLetra } from '../utils/chain.js'
+
 export function Hint({ cadena }) {
   if (cadena.length === 0) {
     return <p className="hint">Empezá con cualquier palabra en español.</p>
   }
 
-  const ultima = cadena[cadena.length - 1]
-  const letra = ultima[ultima.length - 1]
+  const letra = ultimaLetra(cadena[cadena.length - 1])
 
   return <p className="hint">La próxima palabra tiene que empezar con "{letra}".</p>
 }
